b6a2895f740dffdcc12480f873e109853e65425c
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-11 15:03:13 +0200

    adding max widtch

--- src/app/views/HomeView.jsx
+++ src/app/views/HomeView.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { ROLE_ADMIN } from '../constants/rolesConstant';
-import { URL_ADMIN_HOME } from '../constants/urls/urlFrontEnd';
-import { selectHasRole } from '../redux-store/authenticationSlice';
+import { ROLE_ADMIN } from "../constants/rolesConstant";
+import { URL_ADMIN_HOME } from "../constants/urls/urlFrontEnd";
+import { selectHasRole } from "../redux-store/authenticationSlice";
+import Carousel from "../components/Carousel";
 
 const HomeView = () => {
-    const isAdmin = useSelector((state) => selectHasRole(state, ROLE_ADMIN));
-    const navigate = useNavigate();
-    return (
-        <div>
-            <p className="font-extrabold text-primary">HOME</p>
+  const isAdmin = useSelector((state) => selectHasRole(state, ROLE_ADMIN));
+  const navigate = useNavigate();
+  return (
+    <div className="max-w-screen mx-24">
+      <h2 className="uppercase">
+        Les <span className="text-[#349CA2]">nouveautés</span>
+        <br /> actuelles du shop
+      </h2>
+      <Carousel />
 
-            {isAdmin && (
-                <button
-                    className="btn btn-primary"
-                    onClick={() => navigate(URL_ADMIN_HOME)}
-                >
-                    Admin
-                </button>
-            )}
-        </div>
-    );
+      {isAdmin && (
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate(URL_ADMIN_HOME)}
+        >
+          Admin
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default HomeView;
