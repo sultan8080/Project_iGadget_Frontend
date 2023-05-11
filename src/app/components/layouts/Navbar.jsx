796c183f0730e3df11601a71edeea0ff0016aa03
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-11 09:36:19 +0200

    adding search bar and a right section in navbar component

--- src/app/components/layouts/Navbar.jsx
+++ src/app/components/layouts/Navbar.jsx
@@ -13,8 +13,7 @@ const Navbar = () => {
 
   return (
     <div className="absolute mx-auto w-full px-4 sm:px-6">
-      <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
-        
+      <div className="flex items-center justify-between py-6 md:space-x-10">
         {/* LOGO */}
         <div>
           <Link to={URL_HOME}>
@@ -28,11 +27,41 @@ const Navbar = () => {
           </Link>
         </div>
 
+        {/* RIGHT SECTION */}
+        <div className="flex flex-col">
+          ici
+          {/* Search */}
+          <form className="flex items-center">
+            <label for="simple-search" className="sr-only">
+              Search
+            </label>
+            <div className="relative w-full">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="simple-search"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                placeholder="Search"
+                required
+              />
+            </div>
+          </form>
 
-        {/* Search */}
-
-
-
+        </div>
 
         {/* SIGN */}
         {/* <div className="flex flex-1 items-center justify-end lg:w-0">
@@ -51,7 +80,6 @@ const Navbar = () => {
             )}
           </div>
         </div> */}
-
       </div>
     </div>
   );
