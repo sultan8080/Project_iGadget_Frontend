633f6174ddd64f6fd85428532afc6ae24cf7aed2
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-12 10:58:07 +0200

    move carousel in a component

--- src/app/components/CarouselHome.jsx
+++ src/app/components/CarouselHome.jsx
@@ -1,11 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { RxDotFilled } from "react-icons/rx";
 
-const CarouselHome = ({
-  
-}) => {
-  
+const CarouselHome = ({}) => {
+  const slides = [
+    {
+      url: "https://help.apple.com/assets/613FDE81C88FF868634BFD43/613FDE85C88FF868634BFD52/fr_FR/7d204cd60ff1fce24cf6d11f24cd210a.png",
+      alt: "Image 1",
+    },
+    {
+      url: "https://w7.pngwing.com/pngs/5/744/png-transparent-macbook-pro-macbook-air-laptop-macbook-electronics-netbook-computer.png",
+      alt: "Image 1",
+    },
+    {
+      url: "https://www.vhv.rs/dpng/d/7-72850_new-mobile-phone-png-transparent-png.png",
+      alt: "Image 1",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
   return (
-    <div>carousel</div>
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full bg-center bg-cover duration-500"
+      ></div>
+
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
--- src/app/views/HomeView.jsx
+++ src/app/views/HomeView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -6,34 +6,11 @@ import { ROLE_ADMIN } from "../constants/rolesConstant";
 import { URL_ADMIN_HOME } from "../constants/urls/urlFrontEnd";
 import { selectHasRole } from "../redux-store/authenticationSlice";
 
-import { RxDotFilled } from "react-icons/rx";
-
 import CarouselHome from "../components/CarouselHome";
 
 const HomeView = () => {
   const isAdmin = useSelector((state) => selectHasRole(state, ROLE_ADMIN));
   const navigate = useNavigate();
-
-  const slides = [
-    {
-      url: "https://help.apple.com/assets/613FDE81C88FF868634BFD43/613FDE85C88FF868634BFD52/fr_FR/7d204cd60ff1fce24cf6d11f24cd210a.png",
-      alt: "Image 1",
-    },
-    {
-      url: "https://w7.pngwing.com/pngs/5/744/png-transparent-macbook-pro-macbook-air-laptop-macbook-electronics-netbook-computer.png",
-      alt: "Image 1",
-    },
-    {
-      url: "https://www.vhv.rs/dpng/d/7-72850_new-mobile-phone-png-transparent-png.png",
-      alt: "Image 1",
-    },
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
 
   return (
     <div className="max-w-screen mx-24">
@@ -42,26 +19,9 @@ const HomeView = () => {
         <br /> actuelles du shop
       </h2>
 
-      {/* <CarouselHome/> */}
+      <CarouselHome/>
 
-      <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-        ></div>
-
-        <div className="flex top-4 justify-center py-2">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className="text-2xl cursor-pointer"
-            >
-              <RxDotFilled />
-            </div>
-          ))}
-        </div>
-      </div>
+      
 
       {isAdmin && (
         <button
