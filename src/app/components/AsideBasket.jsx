57cbbb33f327062c5a022a7b09e9cff444725e12
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-16 16:54:13 +0200

    adding css in home view and aside basket, font-bold

--- src/app/components/AsideBasket.jsx
+++ src/app/components/AsideBasket.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const AsideBasket = () => {
   return (
-    <aside className="border w-1/4 p-3">
-      <p className="mt-3 text-sm text-gray-500 flex justify-between">
+    <aside className="border w-1/4 p-3 h-auto">
+      <p className="mt-3 text-gray-500 flex justify-between">
         <span>Valeur de la commande</span>
-        <span>1 228</span>
+        <span>1 228 €</span>
       </p>
 
-      <p className="mt-3 text-sm text-gray-500 flex justify-between">
+      <p className="my-3 text-gray-500 flex justify-between">
         <span>Livraison</span>
         <span>GRATUIT</span>
       </p>
 
       <hr />
 
-      <p className="mt-3 text-sm text-gray-500 flex justify-between">
+      <p className="mt-3 text-gray-500 flex justify-between font-bold">
         <span>Total de la commande</span>
-        <span>1 228</span>
+        <span>1 228 €</span>
       </p>
 
       <div className="flex justify-center">
