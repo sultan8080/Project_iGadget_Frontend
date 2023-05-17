2285d3d8bdc3e7a662ae75abb13e307f3054bedf
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-17 10:30:56 +0200

    adding avatar and banner in profile

--- src/app/views/ProfileView.jsx
+++ src/app/views/ProfileView.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const ProfileView = () => {
   return (
-    <div>ProfileView</div>
-  )
-}
+    <>
+      <div class="h-32 overflow-hidden bg-primary"></div>
+      <div class="ml-14 w-36 h-36 relative -mt-16 border-8 border-white rounded-full overflow-hidden">
+        <img
+          class="object-cover object-center h-32"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="Woman looking front"
+        />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+};
 
-export default ProfileView
+export default ProfileView;
