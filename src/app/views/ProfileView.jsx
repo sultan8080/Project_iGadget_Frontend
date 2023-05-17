33cb3f2d87bcd5cf39dbfbcf65c5853f726557eb
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-17 16:20:42 +0200

    changing content in profile

--- src/app/views/ProfileView.jsx
+++ src/app/views/ProfileView.jsx
@@ -22,7 +22,6 @@ const ProfileView = () => {
       <h3 className="-mt-12 ml-56 mb-4">Bonjour Juline !</h3>
 
       <div className="flex flex-col items-center mb-24">
-        
         <section className="flex items-end justify-between border p-6  w-2/3 mt-6">
           <div className="flex flex-col">
             <h5 className="mb-6">Mes Coordonnées</h5>
@@ -35,16 +34,25 @@ const ProfileView = () => {
           </div>
           <button className="btn btn-secondary">Metttre à jour</button>
         </section>
-        
+
         <section className="flex items-end justify-between border p-6  w-2/3 mt-6">
           <div className="flex flex-col">
             <h5 className="mb-6">Mon Carnet D'adresses</h5>
-            {userData.map((data, index) => (
-              <React.Fragment key={index}>
-                <span className="font-bold">{data.label}</span>
-                <span>{data.value}</span>
-              </React.Fragment>
-            ))}
+            <div className="flex">
+              <p className="flex flex-col">
+                <span>Adresse de livraison</span>
+                <span>26 rue d'acheville</span>
+                <span>62 000 Rouvroy</span>
+                <span>France</span>
+              </p>
+
+              <p className="flex flex-col">
+                <span>Adresse de facturation</span>
+                <span>26 rue d'acheville</span>
+                <span>62 000 Rouvroy</span>
+                <span>France</span>
+              </p>
+            </div>
           </div>
           <button className="btn btn-secondary">Metttre à jour</button>
         </section>
@@ -52,12 +60,8 @@ const ProfileView = () => {
         <section className="flex items-end justify-between border p-6  w-2/3 mt-6">
           <div className="flex flex-col">
             <h5 className="mb-6">Confidentialité</h5>
-            {userData.map((data, index) => (
-              <React.Fragment key={index}>
-                <span className="font-bold">{data.label}</span>
-                <span>{data.value}</span>
-              </React.Fragment>
-            ))}
+            <span>Mot de passe</span>
+            <span>*******************</span>
           </div>
           <button className="btn btn-secondary">Metttre à jour</button>
         </section>
@@ -72,7 +76,7 @@ const ProfileView = () => {
               </React.Fragment>
             ))}
           </div>
-          <button className="btn btn-secondary">Metttre à jour</button>
+          <button className="btn btn-primary">Accéder aux commandes</button>
         </section>
 
       </div>
