d757d2e8a84f28781f4cafdb15aea800e97b5fb3
Jessica <jchassagnac@outlook.fr>
2023-05-22 09:41:25 +0200

    création nouveau component

---
+++ src/app/components/ItemsLike.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ItemsLike = () => {
+    return (
+        <div>
+            <div>
+                <h4>
+                    Les articles qui pourraient vous plaire
+                </h4>
+                <hr className='w-64 h-px my-8 border-gray-200 border-0 dark:bg-gray-700 items-center mt-4' />
+            </div>
+
+        </div>
+    );
+};
+
+export default ItemsLike;
--- src/app/views/ProductView.jsx
+++ src/app/views/ProductView.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Product from '../components/Product';
+import ItemsLike from '../components/ItemsLike';
 
 const ProductView = () => {
     return (
         <div>
             <Product />
+            <ItemsLike />
         </div>
     );
 };
