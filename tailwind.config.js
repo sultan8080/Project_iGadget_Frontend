f0f6d1765c566910fb7bb8d8b3f2edf3dec87615
Jessica <jchassagnac@outlook.fr>
2023-05-23 11:36:53 +0200

    install flowbite_react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html', 
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
             colors: {
@@ -23,6 +24,7 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/forms'),
+        require('flowbite/plugin')
     ],
 
 };
