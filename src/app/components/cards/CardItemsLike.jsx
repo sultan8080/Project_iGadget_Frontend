918962e205346939dbc1477adf621fd244cd7876
Jessica <jchassagnac@outlook.fr>
2023-05-25 12:07:21 +0200

    ajustement position photo

--- src/app/components/cards/CardItemsLike.jsx
+++ src/app/components/cards/CardItemsLike.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 function CardItemsLike(){
     return(<>
-<div className="cardItems bg-white w-[200px] h-[240px] m-1 rounded-lg shadow">
-    <div className="top p-3">
-        <img className="w-[150px] h-[100px] object-cover" src="src/app/assets/test/MacBookPro_gris_3.png" alt="image1" />
+<div className="cardItems bg-white w-[250px] h-[300px] m-1 rounded-lg shadow">
+    <div className="top flex justify-center">
+        <img className="w-[125px] h-[200px] object-cover" src="src/app/assets/test/LuminX_bleu.png" alt="image1" />
     </div>
     <div className="bottom flex flex-col justify-center items-start p-3">
         <div className="title font-semibold">
-            MacBookPro
+            Lumin X
         </div>
-        <div className="description text-xs">
+        <div className="description mt-2 text-xs">
         Qui laborum enim optio corporis magnam. Et non quia numquam est dolor qui.
         </div>
     </div>
