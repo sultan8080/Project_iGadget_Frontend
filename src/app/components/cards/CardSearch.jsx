48626e7b5b109e9993f36e34c7bd7721f56c6c4b
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-06-15 15:59:28 +0200

    test result

--- src/app/components/cards/CardSearch.jsx
+++ src/app/components/cards/CardSearch.jsx
@@ -8,11 +8,11 @@ import {
 const CardSearch = ({ result }) => {
   return (
     <div className="border flex h-60 items-center justify-around mt-8">
-      {result.productimages && result.productimages.length > 0 ? (
+      {result.image_name && result.image_name.length > 0 ? (
         <img
           src={URL_BACK_NO_API + URL_BACK_PRODUCT_IMG + "image-1.png"}
           className="w-64 p-4"
-          alt="test"
+          alt={result.name}
         />
       ) : (
         <img
@@ -30,13 +30,13 @@ const CardSearch = ({ result }) => {
       <div className="flex flex-col px-10">
         <button
           type="button"
-          className="text-white bg-[#87D9D4] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 whitespace-nowrap"
+          className="text-white bg-secondary focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 whitespace-nowrap"
         >
           Ajouter au panier
         </button>
         <button
           type="button"
-          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-[#87D9D4] focus:outline-none bg-white rounded-full border border-[#87D9D4] focus:z-10 focus:ring-4 focus:ring-gray-200 whitespace-nowrap"
+          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-secondary focus:outline-none bg-white rounded-full border border-[#87D9D4] focus:z-10 focus:ring-4 focus:ring-gray-200 whitespace-nowrap"
         >
           Voir le produit
         </button>
