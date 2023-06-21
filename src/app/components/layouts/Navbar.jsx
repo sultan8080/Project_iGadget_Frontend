b52497a0e215dd269dc8bc4c3490001a56906b31
Moti-Sultan <motisultannur@gmail.com>
2023-06-21 14:32:53 +0200

    Navbar Admin route crée

--- src/app/components/layouts/Navbar.jsx
+++ src/app/components/layouts/Navbar.jsx
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectIsLogged } from "./../../redux-store/authenticationSlice";
 import {
-  URL_HOME,
-  URL_LOGIN,
-  URL_REGISTER,
-} from "../../constants/urls/urlFrontEnd";
+  selectHasRole,
+  selectIsLogged,
+} from "./../../redux-store/authenticationSlice";
 import SearchInput from "./SearchInput";
 import MainNav from "./MainNav";
+import NavbarAdmin from "./NavbarAdmin";
+import { ROLE_ADMIN } from "../../constants/rolesConstant";
 
 const Navbar = () => {
-  const isLoggued = useSelector(selectIsLogged);
+  const isLogged = useSelector(selectIsLogged);
+  const isAdmin = useSelector((state) => selectHasRole(state, ROLE_ADMIN));
+  if (isAdmin && isLogged) {
+    return <NavbarAdmin />;
+  } else {
+    return (
+      <nav className="flex flex-col justify-end">
+        <div className="flex flex-wrap items-center justify-between ml-32">
+          <a href="/" className="flex items-center">
+            <img
+              src="src/app/assets/logo_iGadget.png"
+              className="h-16 mr-3"
+              alt="logo"
+            />
+          </a>
 
-  return (
-    <nav className="flex flex-col justify-end">
-      <div className="flex flex-wrap items-center justify-between ml-32">
-        <a href="/" className="flex items-center">
-          <img
-            src="src/app/assets/logo_iGadget.png"
-            className="h-16 mr-3"
-            alt="logo"
-          />
-        </a>
+          <div className="bg-gradient-to-r from-[#3495A2] to-[#87D9D4] rounded-bl-full w-3/5 pr-20 pl-60 py-12">
+            <MainNav />
 
-        <div className="bg-gradient-to-r from-[#3495A2] to-[#87D9D4] rounded-bl-full w-3/5 pr-20 pl-60 py-12">
-          <MainNav />
-
-          <SearchInput />
+            <SearchInput />
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 };
 
 export default Navbar;
