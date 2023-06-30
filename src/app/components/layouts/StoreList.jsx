a0dbf0e4f7ad28277b8d494c5931019856fe188d
Moti-Sultan <motisultannur@gmail.com>
2023-06-30 10:06:58 +0200

    NavBar menu primary settings done

--- src/app/components/layouts/StoreList.jsx
+++ src/app/components/layouts/StoreList.jsx
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import apiBackEnd from "../../api/backend/api.Backend";
 import { URL_BACK_CATEGORIES } from "../../constants/urls/urlBackEnd";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsChevronDown } from "react-icons/bs";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -28,31 +30,13 @@ const StoreList = () => {
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:border-0 md:p-0 md:w-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-          </svg>
+          
+          <AiOutlineShoppingCart/>
           <a href="/all-categories" className="block px-4 py-2">
-            Store
+            STORE
           </a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
+          <BsChevronDown/>
+                  
         </Menu.Button>
       </div>
 
